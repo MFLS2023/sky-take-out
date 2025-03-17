25c79e29184d8a1579dbfcbea87f06b96c889e93
MFLS2023 <zc23@zua.edu.cn>
2025-03-17 11:20:48 +0800

    Day_05_修改店铺营业状态和查询店铺营业状态

---
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/ShopServiceImpl.java
@@ -0,0 +1,10 @@
+package com.sky.service.impl;
+
+import com.sky.service.ShopService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ShopServiceImpl implements ShopService {
+}
