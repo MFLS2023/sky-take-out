d4a4833e5f82a503fe91953e62537d02bc2d8670
MFLS2023 <zc23@zua.edu.cn>
2025-03-18 08:50:36 +0800

    Day_06完善微信代码和导入商品浏览功能代码

--- sky-take-out/sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -5,6 +5,7 @@ import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 
 import java.util.List;
