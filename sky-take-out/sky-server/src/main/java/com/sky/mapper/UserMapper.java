e964e5d872a3da781b07236968c81c71fc69a5b9
MFLS2023 <zc23@zua.edu.cn>
2025-03-21 17:55:50 +0800

    Day11_数据统计

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
 
@@ -15,9 +17,19 @@ public interface UserMapper {
     @Select("select * from user where openid = #{openid}")
     User getByOpenid(String openid);
 
+    /**
+     * 插入数据
+     * @param user
+     */
+    void insert(User user);
+
     @Select("select * from user where id = #{id}")
     User getById(Long userId);
 
-    //插入数据
-    void insert(User user);
+    /**
+     * 根据动态条件统计用户数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
